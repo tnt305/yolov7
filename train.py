9c09a963d9dd504283f43bf8dee160dc7839c854
Thien Dang <110881337+tnt305@users.noreply.github.com>
2025-11-02 07:11:49 +0700

    Update train.py

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def train(hyp, opt, device, tb_writer=None):
         if wandb_logger.wandb:
             weights, epochs, hyp = opt.weights, opt.epochs, opt.hyp  # WandbLogger might update weights, epochs if resuming
 
-    nc = 1 if opt.single_cls else int(data_dict['nc'])  # number of classes
+    nc = 1 #if opt.single_cls else int(data_dict['nc'])  # number of classes
     names = ['item'] if opt.single_cls and len(data_dict['names']) != 1 else data_dict['names']  # class names
     assert len(names) == nc, '%g names found for nc=%g dataset in %s' % (len(names), nc, opt.data)  # check
 
